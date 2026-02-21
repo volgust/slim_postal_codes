cd8767d2e81a9406e1a5c774355af4e210559a21
volgustleasoft <maxim.volgust@leasoft.org>
2026-02-21 18:20:47 +0200

    Created script for migrations management, makefile and migration

---
+++ database/migrations/20260220183000_create_locations_table.sql
@@ -0,0 +1,13 @@
+START TRANSACTION;
+    CREATE TABLE locations (
+          id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          region_name VARCHAR(191) NOT NULL,
+          district_name VARCHAR(191) NOT NULL,
+          settlement_name VARCHAR(191) NOT NULL,
+          post_office_name VARCHAR(191) NOT NULL,
+          post_code CHAR(5) NOT NULL,
+          api_created TINYINT(1) DEFAULT 0,
+          UNIQUE KEY uq_post_code (post_code),
+          UNIQUE KEY uq_post_office (region_name, district_name, settlement_name, post_office_name)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
